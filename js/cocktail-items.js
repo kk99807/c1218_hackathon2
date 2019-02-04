9e1b7b956034ce1ea0837738df10c2ed115c1d64
unknown <hannelgwak@gmail.com>
2019-02-03 20:45:01 -0800

    semi-working promises

--- js/cocktail-items.js
+++ js/cocktail-items.js
@@ -9,15 +9,70 @@ class CocktailItems extends PartyItems {
     constructor(domElement, items) {
         super(domElement, items);
         this.bindEvents();
-        $('.alcohol').on('click',this.testSearch)
+        this.doStuff = this.doStuff.bind(this);
+        $('.alcohol').on('click',this.doStuff);
+        this.storeDrinksInArray = this.storeDrinksInArray.bind(this);
+        this.searchDrinkByAlcohol = this.searchDrinkByAlcohol.bind(this);
     }
-    testSearch(){
-        let clickedText = $(this).text();
-        return new Promise((resolve,reject)=>{
-            $.getJson(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${clickedText}`)
+    doStuff(){
+        let spinner = $('<i>').addClass('fa fa-spinner fa-spin');
+        $('.listItemContainer').append(spinner);
+        this.searchDrinkByAlcohol()
+        .then((result)=>this.storeDrinksInArray(result))
+        .then((result2)=>{Promise.all(result2.map(this.getJSON))
+                            .then((result4)=>{result4.forEach(this.storeIngredientData(result4)
+                                //.then(items => items.map(item => item.renderSearch()))
+                                )})})
+        //.then((result3)=>console.log(result3));
+    }
 
-        })
+    searchDrinkByAlcohol(){
+        let clickedText = $(this).text();
+        return new Promise((resolve,reject)=>
+            {resolve($.getJSON(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${clickedText}`))}
+            //.then((json)=>resolve(this.storeDrinksInArray(json)))
+        )
     }
+    storeDrinksInArray(jsonData){
+        let drinksArrayByID = [];
+        for(let jsonDataIndex = 0; jsonDataIndex<10; jsonDataIndex++){
+            let currentDrinkID = jsonData.drinks[jsonDataIndex].idDrink;
+            drinksArrayByID.push(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${currentDrinkID}`)
+        }
+        return drinksArrayByID;
+    }
+    getJSON (url) {  
+        return new Promise((resolve, reject) => {
+          $.getJSON(url)
+            .then((json) => {resolve(json)})
+        });
+    }
+    storeIngredientData(ingredientJsonArray){
+        let items = ingredientJsonArray.map(item => {
+            let ingredients = [];
+            for (let i = 1; i < 16; i++) {
+                let ingredient = item.drinks[0]['strMeasure' + i];
+                let measurement = item.drinks[0]['strIngredient'+i];
+                let fullString = "";
+                if (ingredient && measurement){
+                    fullString = ingredient + '' + measurement
+                    ingredients.push(fullString);
+                } 
+            }
+            return new RecipeItem(
+                item.drinks[0].idDrink, 
+                item.drinks[0].strDrink, 
+                item.drinks[0].strDrinkThumb, 
+                this.handleItemClick, 
+                {ingredients: ingredients, instructions: item.drinks[0].strInstructions}
+            );
+        });
+        return items;
+    }
+    // renderDrinkDetails(drinksArray){
+    //     let drinkArrayCall = drinksArray.map()
+    // }
+
     /**
      * Called by PartyItems superclass when user requests a search to retrieve cocktail recipes
      * @returns {RecipeItem} a Promise to retrieve an array of RecipeItem
