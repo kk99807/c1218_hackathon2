e1d672baa2d90d6669a915681e721e3165678b86
dazcha76 <dazcha@hotmail.com>
2019-05-06 23:35:59 -0700

    removed toast from details, did minor desktop css changes

--- js/party.js
+++ js/party.js
@@ -57,9 +57,6 @@ class Party {
      * Save Event Details and create a calendar entry on user request to update details
      */
     handleUpdateDetails() {
-        M.toast({html:'Details have been saved!', displayLength:1000});
-        $('.toast').css('background-color', 'green');
-
         this.title = $('#inputTitle').val();
         this.date = $('#pickDate').val();
         this.startTime = $('#pickStartTime').val();
